92edec8543de61cf1cd879250bda65cdad45f048
leeclasschic <118270647+leeclasschic@users.noreply.github.com>
2022-11-15 20:30:48 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    //--------------------
+    
+    $('.mag01_slider').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000,
+        fade:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        dots:false,
+        slidesToShow:2,
+    });
+    
+    
+    //--------------------
+    })
